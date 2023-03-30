dd589be538f34b0586a53c32b6b56d97ebf9d881
Anishkvs <anishkumarvijayan@gmail.com>
2023-03-30 13:23:17 +0530

    v0.0.2

---
+++ src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,34 @@
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefination {
+
+	@Given("^User is on NetBanking landing page$")
+	public void user_is_on_NetBanking_landing_page() 
+	{
+		
+	}
+	
+	@When("^User login into application with username and password$")
+	public void user_login_into_application_with_username_and_password()
+	{
+		
+	}
+	
+	@Then("^Home page is populated$")
+	public void Home_page_is_populated()
+	{
+		
+	}
+	
+	@When("^Cards are displayed$")
+	public void Cards_are_displayed()
+	{
+		
+	}
+	
+	
+}
