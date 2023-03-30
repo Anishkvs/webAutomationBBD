66eb02d9a1dd234296f53e49c34592d697d21714
Anishkvs <anishkumarvijayan@gmail.com>
2023-03-30 17:21:50 +0530

    v0.0.4

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -1,34 +1,62 @@
 package stepDefinations;
 
+import java.util.List;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class stepDefination {
 
-	@Given("^User is on NetBanking landing page$")
-	public void user_is_on_NetBanking_landing_page() 
-	{
-		
-	}
-	
-	@When("^User login into application with username and password$")
-	public void user_login_into_application_with_username_and_password()
-	{
-		
-	}
-	
-	@Then("^Home page is populated$")
-	public void Home_page_is_populated()
-	{
-		
-	}
-	
-	@When("^Cards are displayed$")
-	public void Cards_are_displayed()
-	{
-		
-	}
-	
-	
+    @Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+        
+    	System.out.println("Navigate login");
+    }
+
+      
+    @When("^User login into application with \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+        
+    	System.out.println(strArg1);
+    	System.out.println(strArg2);
+    }
+      
+    
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+    	
+    	System.out.println("HomePage");
+    }
+
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are_something(String strArg1) throws Throwable {
+    System.out.println(strArg1);
+    }
+
+    @When("^user signup with following details$")
+    public void user_signup_with_following_details(DataTable data) throws Throwable {
+       List<List<String>> obj = data.asLists();
+       System.out.println(obj.get(0).get(0));
+       System.out.println(obj.get(0).get(1));
+       System.out.println(obj.get(0).get(2));
+       System.out.println(obj.get(0).get(3));
+       System.out.println(obj.get(0).get(4));
+    }
+    
+    @When("^User login in to application with (.+) and (.+) \"([^\"]*)\"$")
+    public void user_login_into_application_with_and_something(String username, String password, String strArg1) throws Throwable {
+        System.out.println(username);
+        System.out.println(password);
+    }
+    
 }
+
+
+
+
+
+
+
--- target/test-classes/features/Login.feature
+++ target/test-classes/features/Login.feature
@@ -2,6 +2,34 @@ Feature: Application login
 
 Scenario: Home page default login
 Given User is on NetBanking landing page
-When User login into application with username and password
+When User login into application with "John" and password "1234"
 Then Home page is populated
-And Cards are displayed
+And Cards displayed are "true"
+
+
+Scenario: Home page default login
+Given User is on NetBanking landing page
+When User login into application with "Mike" and password "4334"
+Then Home page is populated
+And Cards displayed are "flase"
+
+
+Scenario: Home page default login
+Given User is on NetBanking landing page
+When user signup with following details
+| jenny | abcd | john@abcd.com | Australia | 32545455 |
+Then Home page is populated
+And Cards displayed are "false"
+
+Scenario Outline: Home page default login
+Given User is on NetBanking landing page
+When User login in to application with <Username> and <Password> "4334"
+Then Home page is populated
+And Cards displayed are "true"
+
+Examples:
+|Username | Password |
+|user1    | pass 1 |
+|user2    | pass 2 |
+|user 3   | pass 3 |
+
